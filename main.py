93f1d1e275c98d60b1df7863d6140d4e33278843
reinis <reinis.babris@gmail.com>
2016-11-27 11:41:18 +0100

    Changed the URL from ltv7 to ltv2g

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import urllib2
 import re
 
-#1. Atver URL un saglaba response content kaa variable code.
-html = urllib2.urlopen("http://embed.ls.lv/ltv7/index.php") #shis saglabaa response html kaa objektu
+#1. Atver URL un saglaba response content kaa variable code. (vecais URL http://embed.ls.lv/ltv7/index.php)
+html = urllib2.urlopen("http://embed.ls.lv/ltv2g/index.php") #shis saglabaa response html kaa objektu
 code = html.read() #shis atver obejktu kaa string
 
 # 2. Atrod source code dalju, kas atrodas starp vardem iOS un Android. Tur ari atrodas velamais URL.
@@ -12,8 +12,8 @@ code = html.read() #shis atver obejktu kaa string
 ios_list = re.findall('iOS(.*?)Android', code, re.DOTALL)
 ios_string = str(ios_list)
 
-# 3. Taja string dalaa atrodas URL, kas saakas ar velamo sintaksi.
-url_list = re.findall('http://carrier.ls.lv/ltv7/smil:ltv7.smil/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ios_string)
+# 3. Taja string dalaa atrodas URL, kas saakas ar velamo sintaksi. (vecais URL http://carrier.ls.lv/ltv7/smil:ltv7.smil/)
+url_list = re.findall('http://carrier.ls.lv/ltv2g/smil:ltv2g.smil/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ios_string)
 
 # 4. Taa kaa regex output ir liste, tad izejam cauri tas vienigajam itemam un saglabajam ka variable url
 for item in url_list:
