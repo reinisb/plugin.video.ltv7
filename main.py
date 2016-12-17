34b075cd3c786acd6d747d766d896fd42c57767f
reinis <reinis.babris@gmail.com>
2016-12-17 15:59:42 +0100

    Added request header, otherwise had no permissions.

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import urllib2
 import re
 
 #1. Atver URL un saglaba response content kaa variable code. (vecais URL http://embed.ls.lv/ltv7/index.php)
-html = urllib2.urlopen("http://embed.ls.lv/ltv2g/index.php") #shis saglabaa response html kaa objektu
+request = urllib2.Request("http://embed.ls.lv/ltv2g/index.php", headers={"Referer" : "http://ltv.lsm.lv/lv/tieshraide/ltv7/live.782/"}) #sheit nodefinee request URL un obligati ari headeri. Ja nebus shis request headeris, tad lapa atgriezis 403 forbidden.
+html = urllib2.urlopen(request) #shis saglabaa response html kaa objektu
 code = html.read() #shis atver obejktu kaa string
+
+request = urllib2.Request("http://www.google.com", headers={"Accept" : "text/html"})
+contents = urllib2.urlopen(request).read()
 
 # 2. Atrod source code dalju, kas atrodas starp vardem iOS un Android. Tur ari atrodas velamais URL.
 # Taa kaa regex output ir liste [], tad brutali to konvertejam uz parastu string.
@@ -20,7 +24,7 @@ for item in url_list:
   stream_url = item
 
 # 5. Izprintejam URL - tas ir LTV1 striima URL ar visu token. Shis print nav redzams ieksh kodi log.
-# print stream_url
+#print stream_url
 
 # 6. Atskanojam stream URL tiklidz addons tiek atverts.
 xbmc.Player().play(stream_url)
